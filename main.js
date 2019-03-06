56b509987de51db1f0535fb1f49f7e95d936ba5c
ReyAzucar <reyazucar777@gmail.com>
2019-03-06 01:16:51 -0600

    balas enemigos

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ var interval;
 var enemy1 = "./imagenes/cazatie.PNG";
 var enemy2 = "./imagenes/cazatie2.PNG";
 enemyImage = [enemy1, enemy2];
-var health = 10;
 
 var btn = document.querySelector("button");
 
@@ -16,7 +15,7 @@ audio.src = "./Audio/The Millennium Falcon.mp3";
 audio.loop = true;
 
 var endAudioBackground = new Audio();
-endAudioBackground.src = "./Audio/Darth Vader's Theme.mp3";
+endAudioBackground.src = "./Audio/Star Wars Main Theme.mp3";
 endAudioBackground.loop = false;
 
 var endAudioDarth = new Audio();
@@ -27,8 +26,10 @@ class Halcon {
   constructor() {
     this.x = 20;
     this.y = 470;
+    this.health = 10;
     this.width = 100;
     this.height = 120;
+    this.player = true;
     this.image = new Image();
     this.image.src = "./imagenes/halcon.png";
   }
@@ -39,13 +40,17 @@ class Halcon {
 }
 
 class Bullets {
-  constructor(x, y) {
-    this.x = halcon.x + 30;
-    this.y = halcon.y - 20;
+  constructor(x, y, direction = "up") {
+    this.x = direction == "up" ? x + 30 : x;
+    this.y = direction === "up" ? y - 20 : y + 20;
     this.width = 30;
     this.height = 50;
+    this.direction = direction;
     this.image = new Image();
-    this.image.src = "./imagenes/bala-dibujo-png-2.png";
+    this.image.src =
+      this.direction === "up"
+        ? "./imagenes/bala-dibujo-png-2.png"
+        : "./imagenes/bala.png";
   }
   collision(item) {
     return (
@@ -56,63 +61,30 @@ class Bullets {
     );
   }
   draw() {
+    this.direction == "up" ? (this.y -= 7) : (this.y += 7);
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
 }
 
 function drawBullets() {
   bullets.forEach((bullet, i) => {
-    bullet.y -= 7;
     if (bullet.y > canvas.height) {
       return bullets.splice(i, 1);
     }
     bullet.draw();
     enemies.forEach((enemy, index) => {
-      if (bullet.collision(enemy)) {
+      if (bullet.collision(enemy) && bullet.direction !== "down") {
         bullets.splice(i, 1);
         enemies.splice(index, 1);
         score += 100;
       }
     });
-  });
-}
 
-class BulletsEnemy {
-  constructor(x, y) {
-    this.x = enemyImage.x + 30;
-    this.y = enemyImage.y - 20;
-    this.width = 30;
-    this.height = 50;
-    this.image = new Image();
-    this.image.src = "./imagenes/hadouken.png";
-  }
-  collision(item) {
-    return (
-      this.x < item.x + item.width &&
-      this.x + this.width > item.x &&
-      this.y < item.y + item.height &&
-      this.y + this.height > item.y
-    );
-  }
-  draw() {
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-  }
-}
-function drawBalasEnemigo() {
-    bulletsE.forEach((bulletE, i) => {
-    bulletE.y += 7;
-    if (bulletE.y > canvas.height) {
-      return bulletsE.splice(i, 1);
+    if (bullet.collision(halcon) && bullet.direction !== "up") {
+      halcon.health -= 1;
+      bullets.splice(i, 1);
+      if (halcon.health <= 0) clearInterval(interval);
     }
-    bulletE.draw();
-    
-    /*halcon.forEach((halcon, index) => {
-      if (bulletE.collision(halcon)) {
-        bulletsE.splice(i, 1);
-        health--;
-        console.log(health);
-      }
-    });*/
   });
 }
 
@@ -126,6 +98,10 @@ class Enemy {
     this.image.src = img;
   }
 
+  shoot() {
+    bullets.push(new Bullets(this.x, this.y, "down"));
+  }
+
   draw() {
     if (frames % 10) this.x -= 5;
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
@@ -137,13 +113,13 @@ function generateEnemies() {
     const x = Math.floor(Math.random() * 15);
     var randomEnemy = Math.floor(Math.random() * enemyImage.length);
     enemies.push(new Enemy(x * 64, enemyImage[randomEnemy]));
-    bulletsE.push(new BulletsEnemy);
   }
 }
 function drawEnemies() {
   enemies.forEach(function(enemy) {
     enemy.draw();
-    //
+    if (frames % 300 === 0 || frames % 180 == 0 || frames % 100 === 0)
+      enemy.shoot();
   });
 }
 
@@ -158,13 +134,16 @@ class Background {
   }
 
   gameOver() {
+    if (halcon.health === 0){
     clearInterval(interval);
 
     ctx.font = "30px Avenir";
     ctx.fillText("Game Over", 250, 190);
     interval = undefined;
     audio.pause();
+    endAudioBackground.play();
     endAudioDarth.play();
+    }
   }
 
   draw() {
@@ -185,8 +164,6 @@ background = new Background();
 var halcon = new Halcon();
 var bullet = new Bullets();
 let bullets = [];
-let bulletE = new BulletsEnemy();
-let bulletsE = [];
 
 function update() {
   frames++;
@@ -196,11 +173,13 @@ function update() {
   generateEnemies();
   drawEnemies();
   drawBullets();
-  drawBalasEnemigo();
   ctx.fillStyle = "white";
   ctx.font = "25px VT323";
   ctx.fillText("SCORE " + score, 800, 20);
+  background.gameOver();
+
 }
+
 
 function start() {
   if (interval !== undefined) return;
@@ -208,13 +187,13 @@ function start() {
   audio.play();
 }
 
-function restart() {
+/*function restart() {
   if (interval !== undefined) return;
   frames = 0;
   interval = undefined;
   audio.currentTime = 0;
   start();
-}
+}*/
 
 addEventListener("keydown", function(event) {
   if (event.keyCode === 39) {
